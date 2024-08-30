ff1fdf478d8ffb217727c5cd6450928cd576ea5a
Aslan <nissanfordo03@gmail.com>
2024-08-30 17:12:48 +0500

    list of all pixel paintings

--- src/components/pages/PixelListPage.jsx
+++ src/components/pages/PixelListPage.jsx
@@ -2,6 +2,13 @@ import {pixelsData} from "../../data/pixelsData.js";
 
 const PixelListPage = () => {
 
+  const sortedPixels = pixelsData.sort((a, b) => {
+    const pixelA = a.size.w * a.size.h
+    const pixelB = b.size.w * b.size.h
+
+    return pixelB - pixelA
+  })
+
   return (
     <div className={"p-4"}>
       <h4 className={"fw-semibold text-center"}>Список всех изображении на главной канве</h4>
@@ -16,14 +23,13 @@ const PixelListPage = () => {
         </tr>
         </thead>
         <tbody>
-        {pixelsData.map(pixel => {
-          const sizes = pixel.size.split('x')
+        {sortedPixels.map((pixel, index) => {
           return (<tr key={pixel.id}>
-            <th scope="row">{pixel.id}</th>
+            <th scope="row">{index + 1}</th>
             <td>{pixel.title}</td>
             <td>{pixel.url}</td>
-            <td>{pixel.size}</td>
-            <td>{sizes[0] * sizes[1]} пикселей</td>
+            <td>{pixel.size.w}x{pixel.size.h}</td>
+            <td>{pixel.size.w * pixel.size.h} пикселей</td>
           </tr>)
         })}
         </tbody>
